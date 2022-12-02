1b6a88ada1253ba3b5d8bcb7af97b4963df2c260
Andrian <lutsu.andrian@gmail.com>
2022-12-02 12:15:21 +0200

    Init Vector Class

---
+++ Include/Vector/Vector.h
@@ -0,0 +1,11 @@
+
+
+
+class FVector3D
+{
+private:
+    /* data */
+public:
+    FVector3D(/* args */);
+    FVector3D();
+};
---
+++ Source/Vector/Vector.cpp
@@ -0,0 +1,8 @@
+#include "Vector.h"
+
+FVector3D::FVector3D(/* args */)
+{
+}
+ FVector3D::FVector3D()
+{
+}
